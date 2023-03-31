de0c3763832070df9c3b7eb2f314e4f7c540616d
Minor Gordon <minorg@server.fake>
2023-03-31 17:34:41 -0400

    dev.sh: pass

--- dev.sh
+++ dev.sh
@@ -9,6 +9,8 @@ PROJECTS_DIR_PATH=$PWD/../..
 cd $PROJECTS_DIR_PATH/paradicms/lib/py/ssg
 
 poetry run $PROJECTS_DIR_PATH/spreadsheet-ssg-action/action.py \
+  --data-dir-path $PROJECTS_DIR_PATH/paradicms/data/exhibitions \
   --dev \
   --pipeline-id exhibitions \
-  --spreadsheet "1j2oaMvMxY4pnXO-sEH_fky2R2gm6TQeIev_Q8rVOD4M"
+  --spreadsheet "1j2oaMvMxY4pnXO-sEH_fky2R2gm6TQeIev_Q8rVOD4M" \
+  "$@"
