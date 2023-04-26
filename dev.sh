ae01240431e86563a441f99c11180830f46bdfb2
Minor Gordon <minorg@server.fake>
2023-04-26 15:14:56 -0400

    cached -> cache

--- dev.sh
+++ dev.sh
@@ -10,14 +10,14 @@ DATA_DIR_PATH=$PROJECTS_DIR_PATH/paradicms/data/exhibitions
 cd $PROJECTS_DIR_PATH/paradicms/lib/py/ssg
 
 poetry run $PROJECTS_DIR_PATH/spreadsheet-etl-action/action.py \
-  --cache-directory-path $DATA_DIR_PATH/cached \
+  --cache-directory-path $DATA_DIR_PATH/cache \
   --loaded-data-directory-path $DATA_DIR_PATH/loaded \
   --pipeline-id exhibitions \
   --spreadsheet "1j2oaMvMxY4pnXO-sEH_fky2R2gm6TQeIev_Q8rVOD4M" \
   "$@"
 
 poetry run $PROJECTS_DIR_PATH/ssg-action/action.py \
-  --cache-directory-path $DATA_DIR_PATH/cached \
+  --cache-directory-path $DATA_DIR_PATH/cache \
   --data-path $DATA_DIR_PATH/loaded \
   --dev \
   --pipeline-id exhibitions
