0a10bbc1efa196a0c776bb65c87b1a304b0ca797
Minor Gordon <minorg@server.fake>
2023-04-26 11:51:15 -0400

    update dev.sh against paradicms

--- dev.sh
+++ dev.sh
@@ -10,13 +10,14 @@ DATA_DIR_PATH=$PROJECTS_DIR_PATH/paradicms/data/exhibitions
 cd $PROJECTS_DIR_PATH/paradicms/lib/py/ssg
 
 poetry run $PROJECTS_DIR_PATH/spreadsheet-etl-action/action.py \
-  --data-directory-path $DATA_DIR_PATH \
+  --cache-directory-path $DATA_DIR_PATH/cached \
+  --loaded-data-directory-path $DATA_DIR_PATH/loaded \
   --pipeline-id exhibitions \
   --spreadsheet "1j2oaMvMxY4pnXO-sEH_fky2R2gm6TQeIev_Q8rVOD4M" \
   "$@"
 
 poetry run $PROJECTS_DIR_PATH/ssg-action/action.py \
-  --data-directory-path $DATA_DIR_PATH \
+  --cache-directory-path $DATA_DIR_PATH/cached \
+  --data-path $DATA_DIR_PATH/loaded \
   --dev \
   --pipeline-id exhibitions
-
