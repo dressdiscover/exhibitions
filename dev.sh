a652b2e013bb2b9450f1c77a05e809f97676cf87
Minor Gordon <minorg@server.fake>
2023-07-10 13:25:10 -0400

    getting exhibitions working again

--- dev.sh
+++ dev.sh
@@ -4,20 +4,23 @@ set -e
 
 cd `dirname "$(readlink -f "$0")"`
 
+THIS_DIR_PATH=$PWD
+CACHE_DIR_PATH=$THIS_DIR_PATH/.paradicms/cache
+DATA_DIR_PATH=$THIS_DIR_PATH/.paradicms/data
 PROJECTS_DIR_PATH=$PWD/../..
-DATA_DIR_PATH=$PROJECTS_DIR_PATH/paradicms/data/exhibitions
 
 cd $PROJECTS_DIR_PATH/paradicms/lib/py/ssg
 
 poetry run $PROJECTS_DIR_PATH/spreadsheet-etl-action/action.py \
-  --cache-directory-path $DATA_DIR_PATH/cache \
-  --loaded-data-directory-path $DATA_DIR_PATH/loaded \
+  --cache-directory-path $CACHE_DIR_PATH \
+  --force-extract \
+  --loaded-data-directory-path $DATA_DIR_PATH \
   --pipeline-id exhibitions \
   --spreadsheet "1j2oaMvMxY4pnXO-sEH_fky2R2gm6TQeIev_Q8rVOD4M" \
   "$@"
 
 poetry run $PROJECTS_DIR_PATH/ssg-action/action.py \
-  --cache-directory-path $DATA_DIR_PATH/cache \
-  --data-path $DATA_DIR_PATH/loaded \
+  --cache-directory-path $CACHE_DIR_PATH \
+  --data-path $DATA_DIR_PATH \
   --dev \
   --pipeline-id exhibitions
