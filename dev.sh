aacca24ce4c3e3e592df115b6c1bc2e56726464d
Minor Gordon <minorg@server.fake>
2023-10-12 18:52:36 -0400

    update dev.sh

--- dev.sh
+++ dev.sh
@@ -7,11 +7,11 @@ cd `dirname "$(readlink -f "$0")"`
 THIS_DIR_PATH=$PWD
 CACHE_DIR_PATH=$THIS_DIR_PATH/.paradicms/cache
 DATA_DIR_PATH=$THIS_DIR_PATH/.paradicms/data
-PROJECTS_DIR_PATH=$PWD/../..
+PROJECTS_DIR_PATH=$PWD/..
 
 cd $PROJECTS_DIR_PATH/paradicms/lib/py/ssg
 
-poetry run $PROJECTS_DIR_PATH/spreadsheet-etl-action/action.py \
+poetry run $PROJECTS_DIR_PATH/paradicms-spreadsheet-etl-action/action.py \
   --cache-directory-path $CACHE_DIR_PATH \
   --force-extract \
   --loaded-data-directory-path $DATA_DIR_PATH \
@@ -19,8 +19,8 @@ poetry run $PROJECTS_DIR_PATH/spreadsheet-etl-action/action.py \
   --spreadsheet "1j2oaMvMxY4pnXO-sEH_fky2R2gm6TQeIev_Q8rVOD4M" \
   "$@"
 
-poetry run $PROJECTS_DIR_PATH/ssg-action/action.py \
+poetry run $PROJECTS_DIR_PATH/paradicms-ssg-action/action.py \
   --cache-directory-path $CACHE_DIR_PATH \
   --data-path $DATA_DIR_PATH \
-  --dev \
+  --next-commands dev \
   --pipeline-id exhibitions
